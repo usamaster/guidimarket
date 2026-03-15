55345432f461a5ec9e795a941793681f21462ada
usamaster <usamax1@hotmail.com>
2026-03-15 21:59:29 +0100

    add admin

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const ADMIN_USER_ID = '57fa1666-c616-4218-bcff-4fec2e530474'
+export const ADMIN_USER_ID = 'ec2a8840-19cc-420d-8dfb-718398549117'
 export const USERNAME_DOMAIN = 'guidimarket.app'
